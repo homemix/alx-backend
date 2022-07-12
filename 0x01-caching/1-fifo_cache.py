ee7b3b1c97da131d4b63cb83d40090726d709857
homemix <kenmutati@gmail.com>
2022-07-12 11:54:15 +0300

    1. FIFO caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+FIFO Cache module
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ FIFOCache defines:
+      - a FIFO cache system
+      - a max_items constant
+    """
+
+    def __init__(self):
+        """
+        Initialize
+        """
+        super().__init__()
+        self.cache_data_queue = []
+
+    def put(self, key, item):
+        """
+        Add an item in the cache
+        """
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            removed = self.cache_data_queue.pop(0)
+            self.cache_data.pop(removed)
+            print(f'DISCARD: {removed}')
+
+        if key is not None and item is not None:
+            self.cache_data_queue.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Get an item by key
+        """
+        if key is not None:
+            return self.cache_data.get(key)
+        return None
