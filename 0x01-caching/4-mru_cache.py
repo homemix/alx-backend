7d1cae40e1cb8f70bae153089d2674a7e40c1ed6
homemix <kenmutati@gmail.com>
2022-07-13 09:29:47 +0300

    4. MRU Caching

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -15,10 +15,16 @@ class MRUCache(BaseCaching):
     COUNT = 0
 
     def __init__(self):
+        """
+        Initialize the cache class
+        """
         super().__init__()
         self.cache_data_list = []
 
     def put(self, key, item):
+        """
+        Add an item in the cache
+        """
         if key is not None and item is not None and key not in self.cache_data:
             if len(self.cache_data) >= self.MAX_ITEMS:
                 del_key = self.cache_data_list.pop()
@@ -30,13 +36,11 @@ class MRUCache(BaseCaching):
             self.cache_data_list.append(key)
 
     def get(self, key):
+        """
+        Get an item by key
+        """
         if key in self.cache_data_list:
             self.cache_data_list.remove(key)
             self.cache_data_list.append(key)
             return self.cache_data.get(key)
         return None
-
-    def print_cache(self):
-        print("Current cache:")
-        for key in sorted(self.cache_data.keys()):
-            print("{}: {}".format(key, self.cache_data.get(key)))
