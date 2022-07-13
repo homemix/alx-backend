cbc0543a162bb670d40753df399598976c432fb8
homemix <kenmutati@gmail.com>
2022-07-13 09:51:54 +0300

    5. LFU Caching

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -13,17 +13,17 @@ class LFUCache(BaseCaching):
     MAX_ITEMS = 4
     COUNT = 0
 
-    def __int__(self):
+    def __init__(self):
         """
-        initialize class
+        Initiliaze
         """
         super().__init__()
         self.cache_data_list = []
 
     def put(self, key, item):
+        """ Add an item in the cache
         """
-        add an item in the cache
-        """
+        # print(self.cache_data_dict)
         if key is not None and item is not None and key not in self.cache_data:
             if len(self.cache_data) >= self.MAX_ITEMS:
                 del_key = self.cache_data_list.pop(0)
@@ -35,8 +35,7 @@ class LFUCache(BaseCaching):
             self.cache_data_list.append(key)
 
     def get(self, key):
-        """
-        get an item by key
+        """ Get an item by key
         """
         if key in self.cache_data_list:
             self.cache_data_list.remove(key)
