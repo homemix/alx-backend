e7983bc5a6bccb94a634f8a348789cd8b29ebf4f
homemix <kenmutati@gmail.com>
2022-07-19 17:52:45 +0300

    5. Mock logging in

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+"""
+A basic flask app to configure babel
+"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel, gettext
+
+app = Flask(__name__)
+app.template_folder = 'templates'
+babel = Babel(app)
+
+
+class Config(object):
+    """
+    The config class
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user():
+    """
+    Get the user
+    """
+    user_id = request.args.get('login_as')
+    if user_id is None:
+        return None
+    return users.get(int(user_id))
+
+
+@app.before_request
+def before_request():
+    """
+    Before request
+    """
+    user = get_user()
+    g.user = user
+
+
+@babel.localeselector
+def get_locale():
+    """
+    The locale selector
+    """
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """
+    The index page
+    """
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
