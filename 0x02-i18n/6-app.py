ce4e15c6b6b4ae1336f4855e870ce3edcff9d4ba
homemix <kenmutati@gmail.com>
2022-07-20 18:21:29 +0300

    pycodestyle file 6 formatting

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -5,12 +5,11 @@ Module 5-app
 from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
-
 users = {
-        1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
-        2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
-        3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-        4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
 
 
