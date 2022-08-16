4071c4fd89e56c8e77ca9b18c27b3539ab6ce9a8
homemix <kenmutati@gmail.com>
2022-08-16 17:13:53 +0300

    4. Node Redis client and advanced operations

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -1,5 +1,6 @@
 import {createClient} from 'redis';
 import redis from 'redis';
+
 const client = createClient();
 
 client.on('error', (err) => console.log('Redis client not connected to the server:', err));
@@ -10,7 +11,7 @@ const keys = ['Portland', 'Seattle', 'New York', 'Bogota', 'Cali', 'Paris'];
 const values = [50, 80, 20, 20, 40, 2];
 
 keys.forEach((key, index) => {
-  client.HSET('HolbertonSchools', key, values[index], redis.print);
+    client.hset('HolbertonSchools', key, values[index], redis.print);
 });
 
 client.HGETALL('HolbertonSchools', (err, reply) => {
